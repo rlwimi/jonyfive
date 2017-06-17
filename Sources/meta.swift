0adc5c23fae209ae15448acbba60c35a9b2c9ea6
Michael Pederson <rlwimi@gmail.com>
2017-06-17 17:26:38 -0600

    Add `meta` command.

---
+++ Sources/meta.swift
@@ -0,0 +1,20 @@
+import Guaka
+
+var metaCommand = Command(
+  usage: "meta", configuration: configuration, run: execute)
+
+
+private func configuration(command: Command) {
+
+  command.add(flags: [
+    // Add your flags here
+    ]
+  )
+
+  // Other configurations
+}
+
+private func execute(flags: Flags, args: [String]) {
+  // Execute code here
+  print("meta called")
+}
--- Sources/setup.swift
+++ Sources/setup.swift
@@ -2,5 +2,6 @@ import Guaka
 
 // Generated, dont update
 func setupCommands() {
+  rootCommand.add(subCommand: metaCommand)
   // Command adding placeholder, edit this line
 }
