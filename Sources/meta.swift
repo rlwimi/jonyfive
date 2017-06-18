34c3f83e6cc3665126d795e057630366744df567
Michael Pederson <rlwimi@gmail.com>
2017-06-17 21:28:55 -0600

    Loop over supported years, filtering on request.

--- Sources/meta.swift
+++ Sources/meta.swift
@@ -5,22 +5,25 @@ import Kanna
 
 var metaCommand = Command(usage: "meta", configuration: configuration, run: execute)
 
-
 private func configuration(command: Command) {
   command.shortMessage = "Collect session information in a JSON file."
   command.longMessage = "Collect meta information on each session, write it to a JSON file."
 }
 
 private func execute(flags: Flags, args: [String]) {
-  print("calling meta")
 
-  let baseUrl = URL(string: "https://developer.apple.com")!
+  (2012...2017).forEach { year in
+    if let filter = filterYear, year != filter {
+      return
+    }
 
-  guard let doc = HTML(url: baseUrl, encoding: .utf8) else {
-    return
-  }
-
-  if let debug = flags.getBool(name: debug.longName), debug {
-    print("\(doc.title ?? "no title")")
+    guard
+      let yearUrl = URL(string: "https://developer.apple.com/videos/wwdc\(year)"),
+      let doc = HTML(url: yearUrl, encoding: .utf8)
+      else {
+        print("Could not read URL for year \(year)")
+        return
+    }
+    print("\(yearUrl)")
   }
 }
