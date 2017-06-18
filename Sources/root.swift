1aaa1d59b99fab1eada637ac181d0967527e3e31
Michael Pederson <rlwimi@gmail.com>
2017-06-17 20:53:10 -0600

    Add year flag for filtering to a given year.

--- Sources/root.swift
+++ Sources/root.swift
@@ -10,9 +10,27 @@ let debug = Flag(
   inheritable: true
 )
 
+let year = Flag(
+  shortName: "y",
+  longName: "year",
+  type: Int.self,
+  description: "filter by year",
+  required: false,
+  inheritable: true
+)
+
 private func configuration(command: Command) {
   command.longMessage = "Collect public information available at Apple's developer site and act on it in various ways."
-  command.add(flag: debug)
+  command.add(flags: [debug, year])
+
+  command.inheritablePreRun = { flags, args in
+    if let year = flags.getInt(name: year.longName), year < 2012 || year > 2017 {
+      // TODO: can we hook into validation to fail the command?
+      print("Year not supported: \(year)q")
+      return false
+    }
+    return true
+  }
 }
 
 private func execute(flags: Flags, args: [String]) {
