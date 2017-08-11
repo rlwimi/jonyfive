53e22a2967526eba8c422eaa8069113e9acd3469
Michael Pederson <rlwimi@gmail.com>
2017-08-10 19:12:44 -0600

    Meta command accepts an output format flag.

--- Sources/meta.swift
+++ Sources/meta.swift
@@ -3,9 +3,41 @@ import Guaka
 
 var metaCommand = Command(usage: "meta", configuration: configuration, run: execute)
 
+fileprivate let outputFormatOption = Flag(
+  shortName: "f",
+  longName: "format",
+  type: String.self,
+  description: "output format: json (default, for Major Input) or yaml (for ASCIIwwdc)",
+  required: false,
+  inheritable: false
+)
+
+fileprivate enum Format: String {
+  /// Format used by Major Input. Includes all scraped information.
+  case json
+  /// Format used by ASCIIwwdc.com. Includes limited information.
+  case yaml
+}
+
+fileprivate var format: Format = .json
+
 private func configuration(command: Command) {
-  command.shortMessage = "Collect session information in a JSON file."
-  command.longMessage = "Collect meta information on each session, write it to a JSON file."
+  command.shortMessage = "Collect session information in a file."
+  command.longMessage = "Collect meta information on each session, write it to a file."
+
+  command.add(flags: [outputFormatOption])
+
+  command.preRun = { flags, args in
+    guard let formatRawValue = flags.getString(name: outputFormatOption.longName) else {
+      return true
+    }
+    guard let formatValue = Format(rawValue: formatRawValue) else {
+      print("Invalid output format \"\(formatRawValue)\".")
+      return false
+    }
+    format = formatValue
+    return true
+  }
 }
 
 private func execute(flags: Flags, args: [String]) {
