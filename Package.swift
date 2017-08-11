e324a3e9e019d8faa83bff6263a8f4017122b478
Michael Pederson <rlwimi@gmail.com>
2017-08-10 19:25:21 -0600

    Add dependencies for producing yaml for ASCIIwwdc.
    - jpsim/Yams for emitting YAML.
    - IBM-Swift/swift-html-entities for encoding HTML entities.

--- Package.swift
+++ Package.swift
@@ -2,8 +2,10 @@ import PackageDescription
 let package = Package(
   name: "wwwww",
   dependencies: [
-    .Package(url: "https://github.com/oarrabi/Guaka.git", majorVersion: 0),
+    .Package(url: "https://github.com/IBM-Swift/swift-html-entities.git", majorVersion: 3, minor: 0),
+    .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0),
     .Package(url: "https://github.com/oarrabi/FileUtils.git", majorVersion: 0),
+    .Package(url: "https://github.com/oarrabi/Guaka.git", majorVersion: 0),
     .Package(url: "https://github.com/tid-kijyun/Kanna.git", majorVersion: 2),
     ]
 )
