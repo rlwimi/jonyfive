0a2f3fd36a33b3bb79c70b721dc39ae04b895cba
Michael Pederson <rlwimi@gmail.com>
2017-08-11 06:11:49 -0600

    Meta commands emits YAML for yaml option.

--- Sources/meta.swift
+++ Sources/meta.swift
@@ -1,5 +1,7 @@
 import Foundation
 import Guaka
+import HTMLEntities
+import Yams
 
 var metaCommand = Command(usage: "meta", configuration: configuration, run: execute)
 
@@ -41,13 +43,66 @@ private func configuration(command: Command) {
 }
 
 private func execute(flags: Flags, args: [String]) {
+  let sessions = scrapeSessions(filterBy: filterYear, session: filterSession)
+  switch format {
+  case .json:
+    outputJson(for: sessions)
+  case .yaml:
+    outputYaml(for: sessions)
+  }
+}
+
+private func outputJson(for sessions: [Session]) {
   do {
-    let sessions = scrapeSessions(filterBy: filterYear, session: filterSession)
     let dictionaries = sessions.map({ $0.dictionary })
     try JSONSerialization
       .data(withJSONObject: dictionaries, options: .prettyPrinted)
       .write(to: outputPath ?? URL(fileURLWithPath: "./sessions.json"))
   } catch {
     if verboseEnabled { print(error) }
+  }
+}
+
+private func outputYaml(for sessions: [Session]) {
+  do {
+    let yaml = try dump(object: sessions.asciiWwdcYamlObject, width: -1)
+    try yaml.write(to: outputPath ?? URL(fileURLWithPath: "./sessions.yml"), atomically: true, encoding: .utf8)
+  } catch {
+    if verboseEnabled { print(error) }
+  }
+}
+
+extension Array where Element == Session {
+  /// Provides an object emittable as YAML of the form expected by ASCIIwwdc.
+  var asciiWwdcYamlObject: [Node: NodeRepresentable] {
+    var structured: [Node: NodeRepresentable] = [:]
+    forEach { session in
+      let key = Node(session.number)
+      let value = [
+        ":title": Node(session.title.asciiwwdcEscaped),
+        ":track": Node(session.track.rawValue),
+        ":description": Node(session.description.asciiwwdcEscaped)
+      ]
+      structured[key] = value
+    }
+    return structured
+  }
+}
+
+extension String: NodeRepresentable {
+  public func represented() throws -> Node {
+    return Node(self)
+  }
+}
+
+private extension String {
+  /// Mimic escaping found in existing asciiwwdc.com YAML.
+  ///
+  /// Yams emits colon-containing strings in single quotes, but asciiwwdc.com may not expect this.
+  /// asciiwwdc.com may expect HTML enitities and not unicode escape sequences.
+  var asciiwwdcEscaped: String {
+    return self
+      .replacingOccurrences(of: ":", with: "&#58;")
+      .htmlEscape(allowUnsafeSymbols: false, decimal: true, encodeEverything: false, useNamedReferences: true)
   }
 }
